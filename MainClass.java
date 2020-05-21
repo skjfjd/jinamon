d37647f4394b1fffdd7d6500cabe5ef9b06376ac
skjfjd <62280277+skjfjd@users.noreply.github.com>
2020-05-21 20:49:18 +0900

    for문

--- MainClass.java
+++ MainClass.java
@@ -1,34 +1,104 @@
+package forClass;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-
 		/*
-		  random : 난수 ,  무작위   <->	  pattern
-		   1~5
-		   1~45
+		 loop문  순환문
+		 지정한 회수만큼 반복을 하는 제어문
+		 
+		 for , while , do while
+		 
+		 for( 초기화 ; 조건 ; 연산식; ) {
+		 	//처리
+		 for + if	 
+		 	 
+		 int num = 0 ;
+		 num = num +1;
+		 num++;
+		 		
+		 			 
+		 for( 초기화(1) ; 조건문(4)(7)(10);  연산식(3)(6)(9)) {
+		    
+		    	처리(2)(5)(8)
+		   
+		 }
+		 (11) 탈출
 		 */
 		
-		int rnum;
-		// 0~ 4
-		rnum = (int)(Math.random() *5);
-		//System.out.println("rnum : " + rnum);
-		// 0~9
-		rnum = (int)(Math.random() *10);
-		//System.out.println("rnum : " + rnum);
-		// 10 ~ 20
-		rnum = (int)(Math.random() *11 ) + 10;
-		System.out.println("rnum : " + rnum);
-		// 10 20 30 40 50
-		rnum = ((int)(Math.random() *5) + 1) * 10;
-		//System.out.println("rnum : " + rnum);
+		/*int i;
 		
-		// - 1 0 1 
-		rnum = (int)(Math.random() *3) -1;
+		for(i=0;  i<10; i++) {
+			
+			
+		}
+		System.out.println("i=" + i);
+			
+		int j;
+		int num =0;
 		
-		int x = (int)(Math.random() *3) -1;
-		int y = (int)(Math.random()*3) -1;
+		for(j =1; j<=10; j++) {
+			num = num+ j;
+			
+		}
+		System.out.println("num : " + num);*/
+		
+		for(int i =0;  i< 10; i++) {    //루프 안에서도 사용 가능  int 사용 가능하지만 루프 안에서만 사용가능
+			//System.out.println(i);
+			
+		}
+
+		int n;
+		for(n = 0; n<10; n++) {
+			//System.out.println(n);
+		}
+		for(int i= 10;  i>0; i--) {
+		//	System.out.println("i : " + i);
+		}
+		
+		for(int i = 0; i<10; i=i +2) {
+		//	System.out.println("i : " + i );
+		}
+		
+		int count = 0;
+		for( ; count < 10;) {
+		//	System.out.println("count : " + count);
+			count++;
+		}
+		
+		for(int i = 0 , j = 0;  i < 10;  i++ , j++) {
+		//	System.out.println("i : " + i);
+		//	System.out.println("j : "+ j);
+	}
+		
+		//for문 안에 for문
+		
+		// 구구단 2단의 모든 합을 구하라.
+		int sum=0;
+		for(int i = 2;  i<= 20; i = i + 2) {
+		//	System.out.println("i : " + i);
+			sum = sum +i;
+		}
+	//	System.out.println("합 : " +sum);
+		
+		sum = 0 ;
+		int num2 = 2;
+		for(int i= 0 ; i < 10; i++) {
+			sum = sum + num2;
+			num2 = num2 + 2;
+			//System.out.println("합 : " + sum);
+		}
+		System.out.println("합 : " + sum);
+		
+		//2중 for문
+		for(int i= 0; i< 10; i++) { //loop 10
+			//System.out.println("i : " + i);             // 10 x 5  = 50 번 실행 됨
+			
+			for(int j = 0; j <5; j++) { //loop 5
+			//System.out.println("\tj : " + j);
+			//
+		}
 		
 	}
-
+}
 }
