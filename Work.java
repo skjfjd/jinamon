a515b3bb7470a7f30da34e33506feea23c144ee4
skjfjd <62280277+skjfjd@users.noreply.github.com>
2020-05-22 18:02:13 +0900

    과제 5월22일

--- Work.java
+++ Work.java
@@ -1,79 +1,146 @@
+import java.util.Scanner;
 
 public class Work {
 
 	public static void main(String[] args) {
-		
-		int number;
-		
-		number = 5; // 0101
-		
-		number = number <<4 ; //0101 2진법: 0101 0000  16진법 : 80
-		
-		number = number <<5 ; //0101 2진법 1010 0000  16진법 : 160
-				
-		number = number >>1 ; //0101 2진법 0000 0010 16진법 : 2
-		
-		
-		
-		
-		
-		//2번
-		
-		
-		int num1 , num2;
-		
-		num1 =128; // 2진수 1000 0000   16 :80
-		num2 = 56; // 2진수 0011 1000   16 : 38
 
-		System.out.printf("0x%x\n" , num1);
-		System.out.printf("0x%x\n" , num2);
-		//16진수로 바꾼다
 		
+		 // 1부터 ~ 100사이의 숫자
+		
+		//숫자를 입력하라
+		
+		// 
+		int n , n1 ,n2 ,n3, n4, n5, n6 , n7 ,n8 ,n9 ,n10;
+		int quiz = 0;
+		int num = 60;
+		int result = 0;
+		
+	    result = num ;
+	
+		
+			
+		
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.println("첫번째 입력할숫자 : ");
+		n = sc.nextInt();
+		if(n<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n>num){
+			System.out.println("결과값보다 큽니다.");
+		}else if(n==num) {
+			System.out.println("정답입니다.");
+		}
+		
+		System.out.println("두번째 입력할숫자 : ");
+		n1 =sc.nextInt();
+		if(n1<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n1>num){
+			System.out.println("결과값보다 큽니다.");
+		}else if(n1==num) {
+			System.out.println("정답입니다.");
+		}
+		
+		System.out.println("셋번째 입력할숫자 : ");
+		n2 =sc.nextInt();
+		if(n2<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n2>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		
+		System.out.println("넷번째 입력할숫자 : ");
+		n3 =sc.nextInt();
+		if(n3<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n3>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		System.out.println("다섯번째 입력할숫자 : ");
+		n4 =sc.nextInt();
+		if(n4<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n4>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		System.out.println("여섯번째 입력할숫자 : ");
+		n5 =sc.nextInt();
+		if(n5<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n5>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		System.out.println("일곱번째 입력할숫자 : ");
+		n6 =sc.nextInt();
+		if(n6<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n6>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		System.out.println("여덟번째 입력할숫자 : ");
+		n7 =sc.nextInt();
+		if(n7<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n7>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		System.out.println("아홉번째 입력할숫자 : ");
+		n8 =sc.nextInt();
+		if(n8<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n8>num){
+			System.out.println("결과값보다 큽니다.");
+		}
+		System.out.println("열번째 입력할숫자 : ");
+		n9 =sc.nextInt();
+		if(n9<num) {
+			System.out.println("결과 값보다 작습니다.");
+		}else if (n9>num){
+			System.out.println("결과값보다 큽니다.");
+		}else if(result == num){
+			System.out.println("정답입니다.");
+		}
+		
+		
+		
+		
+						
+					
+					/*}else  if(n2<20)    {
+					System.out.println("20보다 작습니다.");
+				}else  if(n3<30)    {
+					System.out.println("30보다 작습니다.");
+				}else  if(n4<40)    {
+					System.out.println("40보다 작습니다.");
+				}else  if(n5<50)    {
+					System.out.println("50보다 작습니다.");
+				}else  if(n6<60)    {
+					System.out.println("60보다 작습니다.");
+				}else  if(n7<70)    {
+					System.out.println("70보다 작습니다.");
+				}else  if(n8<80)    {
+					System.out.println("80보다 작습니다.");
+				}else  if(n9<90)    {
+					System.out.println("90보다 작습니다.");
+				}else  if(n10<100)    {
+					System.out.println("100보다 작습니다.");*/
+				}
+			
+			}
 				
-		num1 = (num1 & num2) | (num1 & num2); //0
-			//	1000 0000
-		    //  0011 1000    & 하나라도 다른 값이면 0
-			//  0000 0000
-		num2 = ~(num1 ^ num2); // 1011 1000
-			//  1000 0000
-			//  0011 1000
-			//  0100 0111  		^ 값 ( 반대로)
-		    //  1011 1000  최종값
-		 	//	
-		System.out.printf("0x%x\n",num1);
-		System.out.printf("0x%x\n",num2);
-		
-		
-		//3번 하나의 수를 입력 받고 삼항 연산자를  사용하여 양수인지 음수 인지 판단 하라
-		
-		String n = "10";
-		
-		n= (number < 5)?"음수":"양수";
-		System.out.println("n= "+ n);
-		
-		
-		//4 번  랜덤수를 취득하라
-		
-		//범위 0 ~ 99 개수 1개
-		
-		int ju;
-		ju = (int)(Math.random() *100);
-		//System.out.println("ju : " + ju);
-		
-		//범위  11 , 12 ,13 ,14 ,15 
-		
-		ju = (int)(Math.random() *5)  +11;
-		//System.out.println("ju : " + ju );
-		
-		// 범위 1~ 45 개수 1개
-		
-		ju = (int)(Math.random() *45)  +1;
-		System.out.println("ju : " + ju );
+			
+			
 		
 		
 		
 		
 		
-	}
+	
 
-}
+
