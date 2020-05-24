367d21a51f481b4e1972111cb1bfd82c8a9c4883
skjfjd <62280277+skjfjd@users.noreply.github.com>
2020-05-24 22:46:18 +0900

    테트리스 게임

--- Study2.java
+++ Study2.java
@@ -1,13 +1,39 @@
+import java.util.Scanner;
 
 public class Study2 {
+
 	public static void main(String[] args) {
+
+		// 나이가 12세 이상  내공이 1400~1800 사이 입장가능 그외엔 입장 불가능
 		
-		System.out.print("=========================================================\n");
-		System.out.print("\\ name\t\tage\t\tphone\t\taddress\t\\\n");
-		System.out.print("=========================================================\n");
-		System.out.print("\\ \"홍길동\" \t20\t\t010-111-2222\t\'경기도\'\t\\\n");
-		System.out.print("\\ \"일지매\" \t18\t\t02-345-4567\t\'서울\'\t\\\n");
-		System.out.print("=========================================================\n");
+		int num1;
+		int num2;
+		
+		int j = 12;
+		int o = 100;
+		
+		int k = 1400;
+		int l = 1800;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("나이 입력 : ");
+		num1 = sc.nextInt();
+		System.out.print("내공 입력 : ");
+		num2 = sc.nextInt();
+		
+		if(j <= o && k<= l) {
+			System.out.println(num1+"세"+num2+"내공" + "입장가능");
+		}else if (j<= o && k<l){
+			System.out.println(num1+"세"+num2+"내공" + "입장불가능");
+		
+		}else if (j <o && k<=l){
+			System.out.println(num1+"세"+num2+"내공" + "입장불가능");
+			
+		}
 	}
-	
 }
+
+	
+	
+
+
