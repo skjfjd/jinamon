a95e817bdbb70f041971c5075941d1dc92c91df6
skjfjd <62280277+skjfjd@users.noreply.github.com>
2020-05-25 20:46:10 +0900

    2차원 배열

--- MainClass.java
+++ MainClass.java
@@ -1,104 +1,50 @@
-package forClass;
 
 public class MainClass {
-
-	public static void main(String[] args) {
-		/*
-		 loop문  순환문
-		 지정한 회수만큼 반복을 하는 제어문
-		 
-		 for , while , do while
-		 
-		 for( 초기화 ; 조건 ; 연산식; ) {
-		 	//처리
-		 for + if	 
-		 	 
-		 int num = 0 ;
-		 num = num +1;
-		 num++;
-		 		
-		 			 
-		 for( 초기화(1) ; 조건문(4)(7)(10);  연산식(3)(6)(9)) {
-		    
-		    	처리(2)(5)(8)
-		   
-		 }
-		 (11) 탈출
-		 */
-		
-		/*int i;
-		
-		for(i=0;  i<10; i++) {
-			
-			
-		}
-		System.out.println("i=" + i);
-			
-		int j;
-		int num =0;
-		
-		for(j =1; j<=10; j++) {
-			num = num+ j;
-			
-		}
-		System.out.println("num : " + num);*/
-		
-		for(int i =0;  i< 10; i++) {    //루프 안에서도 사용 가능  int 사용 가능하지만 루프 안에서만 사용가능
-			//System.out.println(i);
-			
-		}
-
-		int n;
-		for(n = 0; n<10; n++) {
-			//System.out.println(n);
-		}
-		for(int i= 10;  i>0; i--) {
-		//	System.out.println("i : " + i);
-		}
-		
-		for(int i = 0; i<10; i=i +2) {
-		//	System.out.println("i : " + i );
-		}
-		
-		int count = 0;
-		for( ; count < 10;) {
-		//	System.out.println("count : " + count);
-			count++;
-		}
-		
-		for(int i = 0 , j = 0;  i < 10;  i++ , j++) {
-		//	System.out.println("i : " + i);
-		//	System.out.println("j : "+ j);
-	}
-		
-		//for문 안에 for문
-		
-		// 구구단 2단의 모든 합을 구하라.
-		int sum=0;
-		for(int i = 2;  i<= 20; i = i + 2) {
-		//	System.out.println("i : " + i);
-			sum = sum +i;
-		}
-	//	System.out.println("합 : " +sum);
-		
-		sum = 0 ;
-		int num2 = 2;
-		for(int i= 0 ; i < 10; i++) {
-			sum = sum + num2;
-			num2 = num2 + 2;
-			//System.out.println("합 : " + sum);
-		}
-		System.out.println("합 : " + sum);
-		
-		//2중 for문
-		for(int i= 0; i< 10; i++) { //loop 10
-			//System.out.println("i : " + i);             // 10 x 5  = 50 번 실행 됨
-			
-			for(int j = 0; j <5; j++) { //loop 5
-			//System.out.println("\tj : " + j);
-			//
+public static void main(String[] args) {
+	/*
+	 1차원 배열
+	 Array: 같은 자료형의 묶음, 변수들의 묶음
+	 		intdex number로 관리가 된다.
+	 		 0~ n-1 (n은 배열의 총수)
+	 		 
+	 다른 자료형의 묶음 : C언어 > structure(구조세)
+	 				Java > class
+	 				
+	 	
+	 	int array[] = new int[3];
+	 	int array[] = { 1 , 2 , 3, 4 , 5 };
+	 	
+	 	MyStruct my = { 'a' , 24 , "홍길동"};
+	 	
+	 	
+	 	String member[] = { "홍길동" , "24" , "서울시" }
+	 	
+	 	
+	 	2차원 배열
+	 			  가로    세로
+	 	int Array2[3] [4] = {
+	 	{1  , 2  ,   3,   4 },
+	 	{5  , 6   ,  7   , 8},
+	 	{9  , 10  , 11  ,12}
+	 	};
+	 	
+	 	int Array2[][] = new int[3][4];
+	 	int []Array[] = new int[3][4];
+	 */
+	
+	int Array2[][] = {
+			{1 , 2, 3,4},
+			{5,6,7,8},
+			{9,10,11,12}
+	};
+	
+	for (int i = 0; i < Array2.length; i++) {
+		for (int j = 0; j < Array2[0].length; j++) {
+			System.out.println("Array2[" + i + "][" + j + "] = " + Array2[i][j]);
 		}
 		
 	}
+	
+	
 }
 }
